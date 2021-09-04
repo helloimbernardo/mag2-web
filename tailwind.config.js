0dde9137278ad140e844c143a3a52f15f2df128d
Bernardo Oliveira <43864475+bernawastaken@users.noreply.github.com>
2021-09-04 03:01:37 +0100

    First Version of the landing page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    '*.html',
+    '*.js',
+    '*.css'
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
